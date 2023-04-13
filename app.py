044374f414132abdf1bae08d39c4dc52a45f69f2
Livia Alves de Faria <126177124+livialvs@users.noreply.github.com>
2023-04-12 23:31:03 -0300

    Conclusão do Desafio 2, adicionando as devidas rotas e o uso de block.

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from jinja2 import Template
+
+app = Flask("__name__")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/home")
+def home2():
+    return render_template("home.html")
+
+@app.route("/quemsomos")
+def quemsomos():
+    return render_template("quemsomos.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+if __name__ == '__main__':
+    app.run()
